9fa55c110c8c94ecbcf89f293d8935209a8202d0
Haritha-Atmakuri <harithaatmakuri1105@gmail.com>
2019-01-24 17:07:05 -0600

    Formatting the document

--- Program.cs
+++ Program.cs
@@ -5,63 +5,62 @@ namespace p1
     class Program
     {
         static double calculateBmi(double weight, int height)
-        {
-        return weight / ( ( height / 100.0 ) * ( height / 100.0 ) );
-        }
-        static void Main(string[] args)
-        {
-            var name = "";
-            var height = 0;
-            var weight = 0.0;
-            var bmi = 0.0;
-            var bmiValue = "";
-            var gender = "";
-            var checkAgain = true;
-            var cost = 0.0m;
-            var END_CHAR='q';
+          {
+             return weight / ( ( height / 100.0 ) * ( height / 100.0 ) );
+          }
+            static void Main(string[] args)
+           {
+                  var name = "";
+                  var height = 0;
+                  var weight = 0.0;
+                  var bmi = 0.0;
+                  var bmiValue = "";
+                  var gender = "";
+                  var checkAgain = true;
+                  var cost = 0.0m;
+                  var END_CHAR='q';
             
-            while(checkAgain)
-            {
-            Console.WriteLine("Please enter your name:");
-            name = Console.ReadLine();
-            while(name == "")
-            {
-             Console.WriteLine("Please enter your name to proceed forward");
-             name = Console.ReadLine();
-            }
-            Console.WriteLine($"Hello {name}\n********\nWelcome to BMI CALCULATOR APP\n*************");
-            Console.WriteLine("Please enter your height in cms:");
-            height = Convert.ToInt32(Console.ReadLine());
-            while(height <= 0 || height.ToString() == "" )
-            {
-             Console.WriteLine("Please enter valid height to proceed forward");
-             height = Convert.ToInt32(Console.ReadLine());
-            }
-            //Console.WriteLine(height);
-            Console.WriteLine("Please enter your Weight in kgs:");
-            weight = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(weight);
-             while(weight <= 0)
-            {
-             Console.WriteLine("Please enter valid weight to proceed forward");
-             weight = Convert.ToInt32(Console.ReadLine());
-            }
-             Console.WriteLine("Enter your gender as 'm' or 'f':");
-             gender = Console.ReadLine();
+                    while(checkAgain)
+                      {
+                           Console.WriteLine("Please enter your name:");
+                           name = Console.ReadLine();
+                            while(name == "")
+                             {
+                                Console.WriteLine("Please enter your name to proceed forward");
+                                name = Console.ReadLine();
+                             }
+                            Console.WriteLine($"Hello {name}\n********\nWelcome to BMI CALCULATOR APP\n*************");
+                            Console.WriteLine("Please enter your height in cms:");
+                            height = Convert.ToInt32(Console.ReadLine());
+                             while(height <= 0 || height.ToString() == "" )
+                               {
+                                  Console.WriteLine("Please enter valid height to proceed forward");
+                                  height = Convert.ToInt32(Console.ReadLine());
+                               }
+            
+                                 Console.WriteLine("Please enter your Weight in kgs:");
+                                 weight = Convert.ToInt32(Console.ReadLine());
+                                 while(weight <= 0)
+                                    {
+                                       Console.WriteLine("Please enter valid weight to proceed forward");
+                                       weight = Convert.ToInt32(Console.ReadLine());
+                                    }
 
-            bmi = calculateBmi(weight,height);
-            Console.WriteLine($"You bmi is: {bmi}");
-            switch(gender){
-            case "m":
-              if(bmi<20)
-            {
-                bmiValue = "UNDERWEIGHT";
-            }
-            else if(bmi>=20 && bmi<=25)
-            {
-                bmiValue = "NORMAL";
-            }
-            else if(bmi>=25 && bmi<=30)
+                                        Console.WriteLine("Enter your gender as 'm' or 'f':");
+                                        gender = Console.ReadLine();
+                                        bmi = calculateBmi(weight,height);
+                                        Console.WriteLine($"You bmi is: {bmi}");
+                                        switch(gender){
+                                           case "m":
+                                              if(bmi<20)
+                                                {
+                                                   bmiValue = "UNDERWEIGHT";
+                                                }
+                                              else if(bmi>=20 && bmi<=25)
+                                                 {
+                                                    bmiValue = "NORMAL";
+                                                 }
+                                               else if(bmi>=25 && bmi<=30)
             {
                 bmiValue = "OVERWEIGHT";
             }
