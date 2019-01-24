d9067771269f8146005812e0db8f386e41a821b4
Haritha-Atmakuri <harithaatmakuri1105@gmail.com>
2019-01-24 17:34:53 -0600

    done with all testing

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace p1
                   var weight = 0.0;
                   var bmi = 0.0;
                   var bmiValue = "";
-                  var gender = "";
+                  var gender = 'k';
                   var checkAgain = true;
                   var cost = 0.0m;
                   var END_CHAR='q';
@@ -47,11 +47,12 @@ namespace p1
                                     }
 
                                         Console.WriteLine("Enter your gender as 'm' or 'f':");
-                                        gender = Console.ReadLine();
+                                        gender =Convert.ToChar(Console.ReadLine());
                                         bmi = calculateBmi(weight,height);
                                         Console.WriteLine($"You bmi is: {bmi}");
-                                        switch(gender){
-                                           case "m":
+                                        switch(gender)
+                                          {
+                                           case 'm':
                                               if(bmi<20)
                                                 {
                                                    bmiValue = "UNDERWEIGHT";
@@ -61,106 +62,100 @@ namespace p1
                                                     bmiValue = "NORMAL";
                                                  }
                                                else if(bmi>=25 && bmi<=30)
-            {
-                bmiValue = "OVERWEIGHT";
-            }
-            else
-            {
-                bmiValue = "OBESE";
-            }
-            break;
-            case "f":
-              if(bmi<19)
-            {
-                bmiValue = "UNDERWEIGHT";
-            }
-            else if(bmi>=19 && bmi<=24)
-            {
-                bmiValue = "NORMAL";
-            }
-            else if(bmi>=24 && bmi<=29)
-            {
-                bmiValue = "OVERWEIGHT";
-            }
-            else
-            {
-                bmiValue = "OBESE";
-            }
-            break;
-            }
-            Console.WriteLine($"Your weight status is {bmiValue}");
+                                                 {
+                                                    bmiValue = "OVERWEIGHT";
+                                                 }
+                                               else
+                                                 {
+                                                    bmiValue = "OBESE";
+                                                 }
+                                                   break;
+                                               case 'f':
+                                                   if(bmi<19)
+                                                      {
+                                                          bmiValue = "UNDERWEIGHT";
+                                                      }
+                                                   else if(bmi>=19 && bmi<=24)
+                                                     {
+                                                          bmiValue = "NORMAL";
+                                                     }
+                                                   else if(bmi>=24 && bmi<=29)
+                                                      {
+                                                         bmiValue = "OVERWEIGHT";
+                                                      }
+                                                   else
+                                                     {
+                                                         bmiValue = "OBESE";
+                                                     }
+                                                        break;
+                                        }
+                                Console.WriteLine($"Your weight status is {bmiValue}");
            
-            if(bmiValue == "UNDERWEIGHT")
-            {
-                Console.WriteLine($"You are {bmiValue}.\n In order to be fit we recommened you to use this product.\n If you would like to buy this product click 'Y' or else enter 'q' to quit.");
-                var product = Console.ReadLine();
-                if(product == "y")
-                {
-                   Console.WriteLine("The cost for the product is 2 dollars. \n 1.Please enter no of quantity or 'q' to quit.");
-                   var quantity = Console.ReadLine();
-                   if(quantity != "q")
-                   {
-                   cost = 2 * Convert.ToInt32(quantity);
-                   }
-                  if(quantity == "q")
-                 {
-                Console.WriteLine("DONE!");
-                checkAgain = false;
-                break;
-                 }
-                }
-                else if(product == "q")
-            {
-                Console.WriteLine("DONE!");
-                checkAgain = false;
-                break;
-            }
-            Console.WriteLine($"Your order has been placed succesfully with an amount of: {cost} dollars.");
-            }
+                                if(bmiValue == "UNDERWEIGHT")
+                                    {
+                                       Console.WriteLine($"You are {bmiValue}.\n In order to be fit we recommened you to use this product.\n If you would like to buy this product click 'Y' or else enter 'q' to quit.");
+                                       var product = Console.ReadLine();
+                                           if(product == "y")
+                                              {
+                                                Console.WriteLine("The cost for the product is 2 dollars. \n 1.Please enter no of quantity or 'q' to quit.");
+                                                var quantity = Console.ReadLine();
+                                                   if(quantity != "q")
+                                                     {
+                                                        cost = 2 * Convert.ToInt32(quantity);
+                                                     }
+                                                   if(quantity == "q")
+                                                     {
+                                                        Console.WriteLine("DONE!");
+                                                        checkAgain = false;
+                                                        break;
+                                                     }
+                                              }
+                                            else if(product == "q")
+                                             {
+                                                Console.WriteLine("DONE!");
+                                                checkAgain = false;
+                                                break;
+                                             }
+                                            Console.WriteLine($"Your order has been placed succesfully with an amount of: {cost} dollars.");
+                                    }
             
-            if(bmiValue == "NORMAL")
-            {
-                Console.WriteLine("You are perfectly fit and we recommend you to follow the same diet to be healthy like this.");
-            }
+                                if(bmiValue == "NORMAL")
+                                  {
+                                    Console.WriteLine("You are perfectly fit and we recommend you to follow the same diet to be healthy like this.");
+                                  }
 
-            if(bmiValue == "OVERWEIGHT" || bmiValue == "OBESE")
-            {
-                Console.WriteLine($"You are {bmiValue}.\n In order to become thin we recommened you to use this product.\n If you would like to buy this product click 'Y' or else enter 'q' to quit");
-                var product = Console.ReadLine();
-                if(product == "y")
-                {
-                   Console.WriteLine("The cost for the product is 8 dollars. \n 1.Please enter no of quantity or 'q' to quit");
-                   var quantity = Console.ReadLine();
-                   if(quantity!= "q")
-                   {
-                   cost = 8 * Convert.ToInt32(quantity);
-                   }
-                  if(quantity== "q")
-            {
-                Console.WriteLine("DONE!");
-                checkAgain = false;
-                break;
-            }
-                }
-                else if(product == "q")
-            {
-                Console.WriteLine("DONE!");
-                checkAgain = false;
-                break;
-            }
-            Console.WriteLine($"Your order has been placed succesfully with an amount of: {cost} dollars.");
-            }
-            
-            Console.WriteLine("Enter 'y' to check again or 'q' to quit");
-            var checking = Console.ReadLine();
-            END_CHAR=Convert.ToChar(checking);
-            if(END_CHAR=='q')
-            {
-                Console.WriteLine("DONE!");
-                checkAgain = false;
-                break;
-            }
-            }            
-        }
+                                if(bmiValue == "OVERWEIGHT" || bmiValue == "OBESE")
+                                  {
+                                    Console.WriteLine($"You are {bmiValue}.\n In order to become thin we recommened you to use this product.\n If you would like to buy this product click 'Y' or else enter 'q' to quit");
+                                    var product = Console.ReadLine();
+                                      if(product == "y")
+                                        {
+                                           Console.WriteLine("The cost for the product is 8 dollars. \n 1.Please enter no of quantity or 'q' to quit");
+                                           var quantity = Console.ReadLine();
+                                             if(quantity!= "q")
+                                              {
+                                                cost = 8 * Convert.ToInt32(quantity);
+                                              }
+                                            if(quantity== "q")
+                                              {
+                                                Console.WriteLine("DONE!");
+                                                checkAgain = false;
+                                                break;
+                                              }
+                                        }
+                                    
+                                         Console.WriteLine($"Your order has been placed succesfully with an amount of: {cost} dollars.");
+                                 }
+                        Console.WriteLine("Enter 'y' to continue or 'q' to quit");
+                        var checking = Console.ReadLine();
+                        END_CHAR=Convert.ToChar(checking);
+                          if(END_CHAR=='q')
+                             {
+                              Console.WriteLine("DONE!");
+                              checkAgain = false;
+                              break;
+                             }
+                     }            
+             }
     }
 }
